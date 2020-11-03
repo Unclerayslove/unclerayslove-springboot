12afd28f436d463879981a16b89bc90d794d2d19
“leipei” <979803156@qq.com>
2020-11-04 00:11:43 +0800

    thymeleaf整合

---
+++ src/main/java/com/uncleray/controller/web/LoginController.java
@@ -0,0 +1,19 @@
+package com.uncleray.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @program: uncle-ray
+ * @description:
+ * @author: lei pei
+ * @create: 2020-11-03 21:51
+ */
+@Controller
+public class LoginController {
+
+    @GetMapping({"/", "/index"})
+    public String index() {
+        return "index";
+    }
+}
